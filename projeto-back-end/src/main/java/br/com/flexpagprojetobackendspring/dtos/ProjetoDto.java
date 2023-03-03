30ad850bd005e716f6d96ed1252fb1d5ff6678c2
Luiz Gonzaga <luizgonzaga15@gmail.com>
2023-03-03 19:42:23 -0300

    Adicionando a notação do lombok

--- projeto-back-end/src/main/java/br/com/flexpagprojetobackendspring/dtos/ProjetoDto.java
+++ projeto-back-end/src/main/java/br/com/flexpagprojetobackendspring/dtos/ProjetoDto.java
@@ -3,7 +3,9 @@ package br.com.flexpagprojetobackendspring.dtos;
 import jakarta.persistence.Column;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.Data;
 
+@Data
 public class ProjetoDto {
     
     @NotBlank(message = "Nome não pode estar vazio")
