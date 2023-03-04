66459921e768a8e3c45300c6d0b434c066a44075
Luiz Gonzaga <luizgonzaga15@gmail.com>
2023-03-04 04:25:21 -0300

    Colocando pra funcionar a camada de service

--- projeto-back-end/src/main/java/br/com/flexpagprojetobackendspring/controller/ProjetoController.java
+++ projeto-back-end/src/main/java/br/com/flexpagprojetobackendspring/controller/ProjetoController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.flexpagprojetobackendspring.model.Projeto;
 import br.com.flexpagprojetobackendspring.repository.ProjetoRepository;
+import br.com.flexpagprojetobackendspring.service.ProjetoService;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
@@ -31,19 +32,21 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class ProjetoController {
 
+
     @Autowired
-    private final ProjetoRepository projetoRepository;
+    private final ProjetoService projetoService;
 
 
     @GetMapping
     @ResponseBody
-    public List<Projeto> listProjects() {
-        return projetoRepository.findAll();
+    public List<Projeto> listProjects() 
+    {
+        return projetoService.listProjects();
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Projeto> findById(@PathVariable @NotNull @Positive Long id) {
-        return projetoRepository.findById(id)
+        return projetoService.findById(id)
         .map(project -> ResponseEntity.ok().body(project))
         .orElse(ResponseEntity.notFound().build());
     }
@@ -51,27 +54,28 @@ public class ProjetoController {
     @PostMapping
     @ResponseBody
     public ResponseEntity<Projeto> create(@RequestBody @Valid Projeto projeto) {
-        Projeto projetoEntity = projetoRepository.save(projeto);
+        Projeto projetoEntity = projetoService.create(projeto);
         return new ResponseEntity<Projeto>(projetoEntity, HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
     @ResponseBody
-    public ResponseEntity<Projeto> update(@PathVariable @NotNull @Positive Long id, @RequestBody @Valid Projeto projeto) {
-        return projetoRepository.findById(id).map(projetoFound ->{
-            projetoFound.setName(projeto.getName());
-            projetoFound.setCategory(projeto.getCategory());
-            Projeto updated = projetoRepository.save(projetoFound);
-            return ResponseEntity.ok().body(updated);
+    public ResponseEntity<Projeto> update(@PathVariable @NotNull @Positive Long id, 
+    @RequestBody @Valid Projeto projeto) {
+        return projetoService.update(id,projeto).map(projetoFound ->{
+            return ResponseEntity.ok().body(projetoFound);
         }).orElse(ResponseEntity.notFound().build());
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable @NotNull @Positive Long id) {
-        return projetoRepository.findById(id).map(projetoFound -> {
-            projetoRepository.deleteById(id);
+    public ResponseEntity<Void> delete(@PathVariable @NotNull @Positive Long id) 
+    {
+        if(projetoService.delete(id))
+        {
             return ResponseEntity.noContent().<Void>build();
-        }).orElse(ResponseEntity.notFound().build());
+        } else 
+        {
+            return ResponseEntity.notFound().build();
+        }
     }
-
 }
--- projeto-back-end/src/main/java/br/com/flexpagprojetobackendspring/service/ProjetoService.java
+++ projeto-back-end/src/main/java/br/com/flexpagprojetobackendspring/service/ProjetoService.java
@@ -1,62 +1,65 @@
 package br.com.flexpagprojetobackendspring.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
-import br.com.flexpagprojetobackendspring.dtos.ProjetoDto;
 import br.com.flexpagprojetobackendspring.model.Projeto;
 import br.com.flexpagprojetobackendspring.repository.ProjetoRepository;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
 
 @Validated
-@Component
+@Service
 public class ProjetoService {
 
-    @Autowired
-    private ProjetoRepository projetoRepository;
+    
+    private final ProjetoRepository projetoRepository;
+
+    public ProjetoService (ProjetoRepository projetoRepository)
+    {
+        this.projetoRepository = projetoRepository;
+    }
 
     public List<Projeto> listProjects()
     {
         return projetoRepository.findAll();
     }
 
-    public ResponseEntity<Projeto> findById(@PathVariable Long id)
+    public Optional<Projeto> findById(@PathVariable Long id)
     {
-        return projetoRepository.findById(id)
-        .map(project -> ResponseEntity.ok().body(project))
-        .orElse(ResponseEntity.notFound().build());
+        return projetoRepository.findById(id);
     }
 
-    public ResponseEntity<Projeto> create(@RequestBody Projeto projeto)
+    public Projeto create(@Valid Projeto projeto)
     {
-        Projeto projetoEntity = projetoRepository.save(projeto);
-        return new ResponseEntity<Projeto>(projetoEntity, HttpStatus.CREATED);
+        return projetoRepository.save(projeto);
     }
 
-    public ResponseEntity<Projeto> update(@PathVariable Long id, @RequestBody Projeto projeto)
+    public Optional<Projeto> update(@NotNull @Positive Long id, @Valid Projeto projeto)
     {
         return projetoRepository.findById(id).map(projetoFound ->{
             projetoFound.setName(projeto.getName());
             projetoFound.setCategory(projeto.getCategory());
-            Projeto updated = projetoRepository.save(projetoFound);
-            return ResponseEntity.ok().body(updated);
-        }).orElse(ResponseEntity.notFound().build());
+              return projetoRepository.save(projetoFound);
+            });
     }
 
-    public ResponseEntity<Void> delete(@PathVariable Long id)
+    public boolean delete(@PathVariable Long id)
     {
         return projetoRepository.findById(id).map(projetoFound -> {
             projetoRepository.deleteById(id);
-            return ResponseEntity.noContent().<Void>build();
-        }).orElse(ResponseEntity.notFound().build());
+            return true;
+        }).orElse(false);
     }
 
 }
